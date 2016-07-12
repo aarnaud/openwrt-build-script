69439cb24b8b5047fd5844a0225f7b5032b48888
Anthony ARNAUD <perso@anthony-arnaud.fr>
2016-07-12 22:27:03 +0200

    checkout before load groovy script

--- scripts/jenkins.groovy
+++ scripts/jenkins.groovy
@@ -1,10 +1,5 @@
 #!/usr/bin/env groovy
 
-// Mark the code checkout 'stage'....
-stage 'Checkout'
-// Get some code from a GitHub repository
-checkout scm
-
 // Mark the code build 'stage'....
 stage 'Build'
 // Run build script

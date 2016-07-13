532d2976a7f41939a8292a1b5a9b3603dbc7336e
Anthony ARNAUD <github@anthony-arnaud.fr>
2016-07-13 19:16:28 +0200

    Refactoring jenkinsfile

--- scripts/jenkins.groovy
+++ scripts/jenkins.groovy
@@ -1,23 +1,28 @@
 #!/usr/bin/env groovy
 
-// Mark the code build 'stage'....
-stage 'Build'
-// Run build script
-sh "./scripts/build.sh ${env.TARGET}"
+def build(TARGET) {
+    // Mark the code build 'stage'....
+    stage 'Build'
+    // Run build script
+    sh "./scripts/build.sh ${TARGET}"
+}
 
-//Upload artifact
-stage 'Publish artifact'
-def UPLOAD_FILE = "../${env.UPLOAD_FILE}"
-archive "${UPLOAD_FILE}"
+def publishArtifact(UPLOAD_FILE) {
+    //Upload artifact
+    stage 'Publish artifact'
+    archive "${UPLOAD_FILE}"
+}
 
-//Upload on github if tag
-sh "git tag --contains `git rev-parse HEAD` > .git-tag"
-def GIT_TAG = readFile('.git-tag').trim()
-sh "rm .git-tag"
-if (GIT_TAG) {
-    stage 'Publish github release'
-    withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
-        sh "github-release release -u aarnaud -r openwrt-build-script -t ${GIT_TAG}"
-        sh "github-release upload -u aarnaud -r openwrt-build-script -t ${GIT_TAG} -n ${env.ARCHIVE_NAME} -f ${UPLOAD_FILE}"
+def githubRelease(UPLOAD_FILE, ARCHIVE_NAME) {
+    //Upload on github if tag
+    sh "git tag --contains `git rev-parse HEAD` > .git-tag"
+    def GIT_TAG = readFile('.git-tag').trim()
+    sh "rm .git-tag"
+    if (GIT_TAG) {
+        stage 'Publish github release'
+        withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
+            sh "github-release release -u aarnaud -r openwrt-build-script -t ${GIT_TAG}"
+            sh "github-release upload -u aarnaud -r openwrt-build-script -t ${GIT_TAG} -n ${ARCHIVE_NAME} -f ${UPLOAD_FILE}"
+        }
     }
 }

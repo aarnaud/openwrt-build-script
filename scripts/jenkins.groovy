1eac73ab455009c2fe0034864ba29d8356830ff7
Anthony ARNAUD <github@anthony-arnaud.fr>
2021-12-22 00:26:28 -0500

    fix deprecated syntax

--- scripts/jenkins.groovy
+++ scripts/jenkins.groovy
@@ -2,18 +2,20 @@
 
 def build(TARGET) {
     // Mark the code build 'stage'....
-    stage 'Build'
-    def builder = docker.build("openwrt:${env.BUILD_ID}", ".")
-    builder.inside {
-      // Run build script
-      sh "./scripts/build.sh ${TARGET}"
+    stage('Build') {
+      def builder = docker.build("openwrt:${env.BUILD_ID}", ".")
+      builder.inside {
+        // Run build script
+        sh "./scripts/build.sh ${TARGET}"
+      }
     }
 }
 
 def publishArtifact(UPLOAD_FILE) {
     //Upload artifact
-    stage 'Publish artifact'
-    archiveArtifacts artifacts: "${UPLOAD_FILE}"
+    stage('Publish artifact') {
+      archiveArtifacts artifacts: "${UPLOAD_FILE}"
+    }
 }
 
 def githubRelease(UPLOAD_FILE, ARCHIVE_NAME) {
@@ -22,10 +24,11 @@ def githubRelease(UPLOAD_FILE, ARCHIVE_NAME) {
     def GIT_TAG = readFile('.git-tag').trim()
     sh "rm .git-tag"
     if (GIT_TAG) {
-        stage 'Publish github release'
-        withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
-            sh "github-release info -u aarnaud -r openwrt-build-script -t ${GIT_TAG} || github-release release -u aarnaud -r openwrt-build-script -t ${GIT_TAG}"
-            sh "github-release upload -u aarnaud -r openwrt-build-script -t ${GIT_TAG} -n ${ARCHIVE_NAME} -f ${UPLOAD_FILE}"
+        stage('Publish github release') {
+          withCredentials([[$class: 'StringBinding', credentialsId: 'GithubToken', variable: 'GITHUB_TOKEN']]) {
+              sh "github-release info -u aarnaud -r openwrt-build-script -t ${GIT_TAG} || github-release release -u aarnaud -r openwrt-build-script -t ${GIT_TAG}"
+              sh "github-release upload -u aarnaud -r openwrt-build-script -t ${GIT_TAG} -n ${ARCHIVE_NAME} -f ${UPLOAD_FILE}"
+          }
         }
     }
 }

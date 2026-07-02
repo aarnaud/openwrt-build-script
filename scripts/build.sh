e7f1d84f5326e70d0381d7d2bcb1d921dd5ede29
Anthony ARNAUD <github@anthony-arnaud.fr>
2026-07-02 10:10:17 -0400

    update to 25.12.5 and kernel 6.12

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ set -e
 set -x
 
 TARGET=$1
-OPENWRT_VERSION="v24.10.5"
+OPENWRT_VERSION="v25.12.5"
 
 
 SCRIPTS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -39,7 +39,7 @@ git reset --hard HEAD^
 git checkout -f ${OPENWRT_VERSION}
 
 # Patch kernel config to enable nf_conntrack_events
-# patch ${OPENWRT_DIR}/target/linux/generic/config-5.15 < ${ROOT_DIR}/configs/kernel-config.patch
+patch ${OPENWRT_DIR}/target/linux/generic/config-6.12 < ${ROOT_DIR}/configs/kernel-config.patch
 
 rm -rf ${OPENWRT_DIR}/files
 cp -r ${ROOT_DIR}/root_files ${OPENWRT_DIR}/files
@@ -69,4 +69,3 @@ fi
 make download
 
 make -j$(nproc) || make V=s # Retry with full log if failed
-
